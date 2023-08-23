4fb3107c9cce5f87c97a1a07cff30f93aa168a9a
Johar Terraza <0318118719@miutt.edu.mx>
2023-08-23 09:23:36 -0700

    Implement weather card

---
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { CurrentWeather } from "../../interfaces/weather";
+
+interface WeatherCardProps {
+    weatherInfo: CurrentWeather | null
+}
+
+const WeatherCard: React.FC<WeatherCardProps> = ({weatherInfo}) => {
+    return (
+        <div className="weatherCard">
+            <p>City: {weatherInfo?.cityName}</p>
+            <p>Temp: {weatherInfo?.currentTemp}</p>
+            <p>Max temp: {weatherInfo?.maxTemp}</p>
+            <p>Min temp: {weatherInfo?.minTemp}</p>
+            <p>Humidity: {weatherInfo?.humidity}</p>
+        </div>
+    )
+}
+
+export default WeatherCard;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherInput from "../../components/WeatherInput/WeatherInput";
 import { CurrentWeather } from "../../interfaces/weather";
+import WeatherCard from "../../components/WeatherCard/WeatherCard";
 
 const Home: React.FC = () => {
+    const [isSearchTriggered, setIsSearchTriggered] = useState<Boolean>(false);
+    const [cityResult, setCityResult] = useState<CurrentWeather | null>(null);
     
     function showData(data: CurrentWeather | null) {
-       console.log(data)
+       setCityResult(data);
+       setIsSearchTriggered(true);
     }
+
     return (
         <div className="homePage">
             <WeatherInput searchHandler={(data) => showData(data)}/>
+            {isSearchTriggered && (
+                <aside>                    
+                    {cityResult ? (
+                        <WeatherCard weatherInfo={cityResult}/>
+                    ) : (
+                        <p>No city result available.</p>
+                    )}
+                </aside>
+            )}
         </div>
     )
 }
